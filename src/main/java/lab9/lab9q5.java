e51bad443714d3e7347e3aa31d0a6070dbd192be
ajeetverma375 <ajeetverma375@gmail.com>
2021-11-17 09:23:21 +0530

    c

--- src/main/java/lab9/lab9q5.java
+++ src/main/java/lab9/lab9q5.java
@@ -1,7 +1,23 @@
 package lab9;
+
+
+import java.util.List;
+import java.util.stream.Stream;
+
 //Write a method to calculate factorial of a number. Test this method using method reference feature. 
 public class lab9q5 
 {
+    public static List<Integer> getFibonacci(int series) {
+        return Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(series).map(n -> n[0]).collect(toList());
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> fibonacci = getFibonacci(10);
+        fibonacci.forEach(x -> System.out.println(x));
+
+    }
 
 }
 
