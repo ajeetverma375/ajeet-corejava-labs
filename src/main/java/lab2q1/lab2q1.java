d537fd2e7875887fcc7f57b524a9d0443169b000
Ajeet_Verma <Ajeet_Verma@epam.com>
2021-12-14 21:13:49 +0530

    c

--- src/main/java/lab2q1/lab2q1.java
+++ src/main/java/lab2q1/lab2q1.java
@@ -5,27 +5,21 @@ import java.util.Scanner;
 
 // print second largest number from array
 
-public class lab2q1 
-{
+public class lab2q1 {
 
-	public int secondSmallest(int[] arr) 
-	{
+	public int secondSmallest(int[] arr) {
 		Arrays.sort(arr);
-return arr[1];
+		return arr[1];
 	}
-    
-	
-	
-	
-	//Driver 
-	public static void main(String[] args)
-	{
+
+	// Driver
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter length: ");
 		int n = sc.nextInt();
 		int[] arr = new int[n];
-		for (int i = 0; i < n; i++)
-		{System.out.println("enter value");
+		for (int i = 0; i < n; i++) {
+			System.out.println("enter value");
 			arr[i] = sc.nextInt();
 		}
 		lab2q1 ref = new lab2q1();
